eb709d4d86689dfffcbc83e0db65ba92e8a6a033
chadipiralla2428 <schadipiralla@gmail.com>
2023-07-28 15:14:37 +0530

    war file changes

--- src/main/java/com/example/devopsdemo/DevopsdemoApplication.java
+++ src/main/java/com/example/devopsdemo/DevopsdemoApplication.java
@@ -2,6 +2,7 @@ package com.example.devopsdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,10 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 @RequestMapping("/devOps")
-public class DevopsdemoApplication {
+public class DevopsdemoApplication extends SpringBootServletInitializer {
 	@GetMapping("/get")
 	public String demo() {
-		return "jenkins demo";
+		return "welcome to devOps";
 	}
 
 	public static void main(String[] args) {
